fc391db21de17365f0901a4f7dbb6ec3f326a070
Lucasdias79931 <lucasdias26@hotmail.com.br>
2024-10-10 16:06:55 -0300

    ATUALIZADO

--- alingWithMafft.py
+++ alingWithMafft.py
@@ -2,14 +2,23 @@ import subprocess
 import os
 import time
 
-def alignWithMafft(reference_file, fragments_file, output):
 
+def alignWithMafft(reference_file, fragments_file, output):
+    # Cria o diretório de saída, se não existir
     os.makedirs(os.path.dirname(output), exist_ok=True)
 
+    # Monta o comando para MAFFT
     command = ["mafft", "--add", fragments_file, reference_file]
 
-    with open(output, "a") as file:
-        subprocess.run(command, stdout=file, check=True)
+    try:
+        # Executa o comando e redireciona a saída para o arquivo de saída
+        with open(output, "w") as file:  # Usar "w" para sobrescrever o arquivo se existir
+            subprocess.run(command, stdout=file, check=True)
+        print(f"Alinhamento concluído e salvo em: {output}")
+    except subprocess.CalledProcessError as e:
+        print(f"Erro ao executar o MAFFT: {e.stderr.decode('utf-8')}")
+    except Exception as e:
+        print(f"Erro inesperado: {e}")
 
 ################################ Executar #########################
 base_directory = os.getcwd()
